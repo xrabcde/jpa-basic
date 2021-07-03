f7f8e5271469463cb775ef005987f2d4c7b1c3df
xrabcde <xrabcde@gmail.com>
2021-07-03 21:07:28 +0900

    feat: Member와 Team 객체 연관관계 매핑

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -24,7 +24,7 @@ public class JpaMain {
             //회원 등록
             Member member = new Member();
             member.setName("HelloA");
-            member.setTeamId(team.getId());
+            member.setTeam(team); //단방향 연관관계 설정, 참조 저장
 
             em.persist(member);
 
@@ -34,14 +34,17 @@ public class JpaMain {
 //            em.persist(member2);
 
             //회원 조회
-            final Member findMember = em.find(Member.class, 2L);
-            final List<Member> findMembers = em.createQuery("select m from Member as m", Member.class).getResultList();
+            final Member findMember = em.find(Member.class, member.getId());
+//            final List<Member> findMembers = em.createQuery("select m from Member as m", Member.class).getResultList();
+
+            //팀 조회
+            Team findTeam = findMember.getTeam();
 
             //회원 삭제
 //            em.remove(findMember);
 
             //회원 수정
-            findMember.setName("HelloJPA"); //persist() 안 해줘도 됨
+            findMember.setName("Hello"); //persist() 안 해줘도 됨
 
             tx.commit();
         } catch (Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -11,8 +11,9 @@ public class Member {
     @Column(name = "USERNAME")
     private String name;
 
-    @Column(name = "TEAM_ID")
-    private Long teamId;
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -30,11 +31,11 @@ public class Member {
         this.name = name;
     }
 
-    public Long getTeamId() {
-        return teamId;
+    public Team getTeam() {
+        return team;
     }
 
-    public void setTeamId(Long teamId) {
-        this.teamId = teamId;
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
